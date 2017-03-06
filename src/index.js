69874e92df3293708ccf27f29605826e3697b371
Megan Coyle <meg@mcoyle.com>
2017-03-05 20:11:45 -0500

    Linking redux files with app

--- src/index.js
+++ src/index.js
@@ -5,17 +5,32 @@ import './stylesheets/index.scss';
 import { App } from './components/App';
 import { Whoops404 } from './components/Whoops404';
 import { Router, Route, hashHistory } from 'react-router';
+import C from './constants';
+import { allTravelDays, goal } from './initialState.json';
 
-window.React = React
+console.log(`
 
-render(
-	<Router history={hashHistory}>
-		<Route path="/" component={App}/>
-		<Route path="list-days" component={App} >
-			<Route path=":filter" component={App} />
-		</Route>
-		<Route path="add-day" component={App} />
-		<Route path="*" component={Whoops404}/>
-	</Router>,
-	document.getElementById('react-container')
-)
+   Travel Day Counter
+   ================
+   The goal is ${goal} days
+   Initially there are ${allTravelDays.length} travel days in state
+
+   Constants (actions)
+   -------------------
+   ${Object.keys(C).join('\n     ')}
+
+`)
+
+// window.React = React
+//
+// render(
+// 	<Router history={hashHistory}>
+// 		<Route path="/" component={App}/>
+// 		<Route path="list-days" component={App} >
+// 			<Route path=":filter" component={App} />
+// 		</Route>
+// 		<Route path="add-day" component={App} />
+// 		<Route path="*" component={Whoops404}/>
+// 	</Router>,
+// 	document.getElementById('react-container')
+// )
